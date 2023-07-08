0fc3aafb44de7ab373d50b85db556e0461474031
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-08 19:12:06 +0100

    Initial file

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,4 @@
+user_name = input("Type your name: ")
+print("Welcome", user_name, "to your adventure")
+
+answer = input("Your are on a dirt road, it has come to the end and you go left or right. Where you go? ")
