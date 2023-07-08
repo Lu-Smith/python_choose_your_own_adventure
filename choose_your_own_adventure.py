f9f111564fc2cfb3828edd04993f44541dd85ecb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-08 19:48:21 +0100

    Updated logic

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -6,14 +6,32 @@ answer = input("Your are on a dirt road, it has come to the end and you go left 
 if answer == "left":
     answer = input("You come to a river, you can walk around it or swim accross? Type walk to walk, swim to swim across. ")
 
-    if answer == "walk":
+    if answer == "swim":
         print("You swam accross and were eaten by a crocodile.")
-    elif answer == "swim":
+    elif answer == "walk":
         print("You walked for many miles, ran out of water and you lost the game.")
     else:
         print("Not a valid option. You loose. ")
 
 elif answer == "right":
-    print("")
+    answer = input("You come to a bridge it looks wobbly, do you want to cross it or head back? (cross/back)")
+
+    if answer == "cross":
+        answer = input("You cross the bridge and you meet a stranger. Do you talk to them? (yes/no)")
+
+        if answer == "yes":
+            print("You talk to the stranger and they gave you a gold. You won!")
+        elif answer == "no":
+            print("You ignore the stranger, and he killed you. You lost.")
+        else:
+            print("Not a valid option. You loose. ")
+
+    elif answer == "back":
+        print("You go back and loose.")
+    else:
+        print("Not a valid option. You loose. ")
 else:
     print("Not a valid option. You loose. ")
+
+
+print("Thank you for trying!")
